fd26b7dc739e1e4009d6d568e053adba9eafb8f8
Cody Malick <cody.malick@gmail.com>
2015-11-30 18:55:35 -0800

    Added basic class files, created basic view, removed debug files

--- EclipseZebra/EclipseZebra/Program.cs
+++ EclipseZebra/EclipseZebra/Program.cs
@@ -16,7 +16,7 @@ namespace EclipseZebra
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainScreen());
         }
     }
 }
