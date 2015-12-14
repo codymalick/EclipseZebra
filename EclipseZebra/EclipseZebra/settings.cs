0456c3528d1b7c29db3d9f288689a9210624f0b2
codymalick <cody.malick@gmail.com>
2015-12-13 19:22:26 -0800

    Test print dialog button

--- EclipseZebra/EclipseZebra/settings.cs
+++ EclipseZebra/EclipseZebra/settings.cs
@@ -33,5 +33,17 @@ namespace EclipseZebra
                 
                 
         }
+
+        private void PrinterTB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PrintDialog print = new PrintDialog();
+            print.ShowDialog();
+            
+        }
     }
 }
